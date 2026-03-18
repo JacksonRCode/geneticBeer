050b5866ac5b3fea7341ab85541b1cd8a507a65b
15kp20 <talk2kevinp@gmail.com>
2026-03-18 19:10:02 -0400

    normalized fitness functions

--- fitness.py
+++ fitness.py
@@ -3,6 +3,7 @@ from dataStrucs import Recipe
 
 def fitness_distance(recipe: Recipe, target):
     """
+    Normalized
     Fitness based on squared distance to target OG, IBU, SRM.
     Returns fitness
     """
@@ -10,9 +11,34 @@ def fitness_distance(recipe: Recipe, target):
     ibu = recipe.calculate_ibu()
     srm = recipe.calculate_srm()
 
-    og_diff = target["og"] - og
-    ibu_diff = target["ibu"] - ibu
-    srm_diff = target["srm"] - srm
+    #normalize values
+    og_range = 0.1 #difference between minimum of all og values in beer_styles.csv and max of all og values
+    ibu_range = 100 #same as above, for ibu
+    srm_range = 38.5
+
+    og_diff = (target["og"] - og)/og_range
+    ibu_diff = (target["ibu"] - ibu)/ibu_range
+    srm_diff = (target["srm"] - srm)/srm_range
+
+    fitness = ((og_diff ** 2) + (ibu_diff ** 2) + (srm_diff ** 2))**0.5
+
+    recipe.fitness_score = fitness #store the fitness
+
+    return fitness
+
+
+def fitness_distance_unormalized(recipe: Recipe, target):
+    """
+    Fitness based on squared distance to target OG, IBU, SRM.
+    Returns fitness
+    """
+    og = recipe.calculate_original_gravity()
+    ibu = recipe.calculate_ibu()
+    srm = recipe.calculate_srm()
+
+    og_diff = (target["og"] - og)
+    ibu_diff = (target["ibu"] - ibu)
+    srm_diff = (target["srm"] - srm)
 
     fitness = ((og_diff ** 2) + (ibu_diff ** 2) + (srm_diff ** 2))**0.5
 
@@ -22,6 +48,7 @@ def fitness_distance(recipe: Recipe, target):
 
 def fitness_range_penalty(recipe: Recipe, target_range):
     """ 
+    Normalized
     Range-based penalty fitness function.
     Returns fitness_score
     """
@@ -33,23 +60,28 @@ def fitness_range_penalty(recipe: Recipe, target_range):
     ibu_min, ibu_max = target_range["ibu"]
     srm_min, srm_max = target_range["srm"]
 
+    #normalize values
+    og_range = og_max - og_min
+    ibu_range = ibu_max - ibu_min
+    srm_range = srm_max - srm_min
+
     og_penalty = 0
     if og < og_min:
-        og_penalty = (og_min - og) ** 2
+        og_penalty = ((og_min - og)/og_range) ** 2
     elif og > og_max:
-        og_penalty = (og - og_max) ** 2
+        og_penalty = ((og - og_max)/og_range) ** 2
 
     ibu_penalty = 0
     if ibu < ibu_min:
-        ibu_penalty = (ibu_min - ibu) ** 2
+        ibu_penalty = ((ibu_min - ibu)/ibu_range) ** 2
     elif ibu > ibu_max:
-        ibu_penalty = (ibu - ibu_max) ** 2
+        ibu_penalty = ((ibu - ibu_max)/ibu_range) ** 2
 
     srm_penalty = 0
     if srm < srm_min:
-        srm_penalty = (srm_min - srm) ** 2
+        srm_penalty = ((srm_min - srm)/srm_range) ** 2
     elif srm > srm_max:
-        srm_penalty = (srm - srm_max) ** 2
+        srm_penalty = ((srm - srm_max)/srm_range) ** 2
 
     fitness = og_penalty + ibu_penalty + srm_penalty
 
